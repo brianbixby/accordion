21c10f81a448e61fdd8d686bc1cfa99803546994
Brian Bixby <brianbixby0@gmail.com>
2018-01-13 20:36:18 -0800

    fixed accordion so when you click on a title with a open body the self toggle works

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 $('.accordion').on('click', '.accordionTitleDiv', function() {
-    $('.accordion').find('.show').removeClass('show').addClass('hide');
-    $(this).parent().find('.accordionContent').removeClass('hide').addClass('show');
+    if($(this).parent().find('.accordionContent').hasClass('show')) {
+        $(this).parent().find('.accordionContent').removeClass('show').addClass('hide');
+    }
+    else {
+        $('.accordion').find('.show').removeClass('show').addClass('hide');
+        $(this).parent().find('.accordionContent').removeClass('hide').addClass('show');
+    }
 });
